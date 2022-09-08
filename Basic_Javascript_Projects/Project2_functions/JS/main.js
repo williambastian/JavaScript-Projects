a90c878fbe0426a15f6fdfe4007855989256c708
William <87213388+williambastian@users.noreply.github.com>
2022-09-08 17:31:18 -0500

    Created Python-Projects README

--- Basic_Javascript_Projects/Project2_functions/JS/main.js
+++ Basic_Javascript_Projects/Project2_functions/JS/main.js
@@ -10,3 +10,8 @@ function concatFunction() {
     sentence += " a lot from this course.";
     document.getElementById("Concatenate").innerHTML = sentence;
 }
+
+function testFunction(a, b){
+    document.getElementById("answer").innerHTML = a * b;
+
+}
